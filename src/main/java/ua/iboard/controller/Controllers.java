6566932b3b4a10d256b7cb231983bb8f19ece850
Yechen Chypachenko <echipachenko@gmail.com>
2017-07-15 10:41:14 +0300

    add controllers holder

---
+++ src/main/java/ua/iboard/controller/Controllers.java
@@ -0,0 +1,37 @@
+package ua.iboard.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * Created by Yevhen Chypachenko
+ * Date 15.07.2017
+ */
+public class Controllers {
+    private static final Logger log = Logger.getLogger(Controllers.class.getName());
+    private static Controllers ourInstance = new Controllers();
+
+    public static Controllers getInstance() {
+        return ourInstance;
+    }
+
+    private final Map<String, IController> controllers = new HashMap<>();
+
+    private Controllers() {
+        controllers.put("/", new MainPageController());
+    }
+
+    public void handle(String target, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        log.info("Handling request: " + req);
+        IController controller = controllers.get(target);
+        if (controller == null){
+            throw new ServletException("No register controller found for target: " + target );
+        }
+
+        controller.handle(req, resp);
+    }
+}
--- src/main/java/ua/iboard/http/IBoardHandler.java
+++ src/main/java/ua/iboard/http/IBoardHandler.java
@@ -2,44 +2,34 @@ package ua.iboard.http;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import ua.iboard.controller.MainPageController;
+import ua.iboard.controller.Controllers;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Logger;
+import java.rmi.ServerException;
 
 /**
  * Created by Yevhen Chypachenko
  * Date 13.07.2017
  */
 public class IBoardHandler extends AbstractHandler {
-    private static final Logger log = Logger.getLogger(IBoardHandler.class.getName());
+    private Controllers controllers = Controllers.getInstance();
 
     @Override
     public void handle(String target,
                        Request request,
                        HttpServletRequest httpServletRequest,
                        HttpServletResponse httpServletResponse) throws IOException, ServletException {
-        log.info("request" + request);
 
         httpServletResponse.setCharacterEncoding("UTF-8");
 
-        String method = request.getMethod();
-        if (method.equals("GET")) {
-            log.info("On GET");
-            if (target.equals("/")) {
-                try {
-                    new MainPageController().handle(httpServletRequest, httpServletResponse);
-                    request.setHandled(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        } else if (method.equals("POST")) {
-
+        try {
+            controllers.handle(target, httpServletRequest, httpServletResponse);
+            request.setHandled(true);
+        } catch (Exception e) {
+            throw new ServerException("An error occured", e);
         }
     }
 }
